3b85cbee7405e755c86d72beed3d54a688359abe
Sebastian-Betancourt <168014602+Sebastian-Betancourt@users.noreply.github.com>
2025-06-02 15:03:38 -0500

    Delete QuitoEmprende directory

--- QuitoEmprende/backend-vet/src/routes/administradorRoutes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { registrarAdministrador } from '../controllers/administradorController.js';
-
-const router = Router();
-
-// POST /api/administradores/registro
-router.post('/registro', registrarAdministrador);
-
-export default router;
--- QuitoEmprende/backend-vet/src/routes/clienteRoutes.js
+++
@@ -1,9 +0,0 @@
-// routes/clienteRoutes.js
-import express from 'express';
-import { registrarCliente } from '../controllers/clienteController.js';
-
-const router = express.Router();
-
-router.post('/registrar', registrarCliente);
-
-export default router;
